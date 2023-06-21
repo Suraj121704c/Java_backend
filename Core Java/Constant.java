0dbc195d67d9a6db881856fa198f7a00415c79d8
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-21 23:08:20 +0530

    Static Vs Non-Static

---
+++ Core Java/Constant.java
@@ -0,0 +1,11 @@
+public class Constant {
+    public static final int MAX_VALUE = 100;
+    public static final double PI = 3.14159;
+    public static final String MESSAGE = "Hello, world!";
+
+    public static void main(String[] args) {
+        System.out.println("Max value: " + MAX_VALUE);
+        System.out.println("Pi: " + PI);
+        System.out.println("Message: " + MESSAGE);
+    }
+}
