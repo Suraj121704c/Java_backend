66337d90a34d440b00634b8451c35366e9cf8174
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-21 17:30:04 +0530

    abstract

--- Core Java/AbstractCalling.java
+++ Core Java/AbstractCalling.java
@@ -1,5 +1,9 @@
 public class AbstractCalling {
     public static void main(String[] args) {
+        AbstractDemo noExtend = new AbstractExtend1();
+        noExtend.draw();
+        noExtend.resize();
+
         AbstractExtend1 extend1 = new AbstractExtend1();
         extend1.draw();
         extend1.resize();
