6c9f493e2c993788375e4fe8f9a55732369d71dd
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-21 06:59:47 +0530

    setters

---
+++ Core Java/StudentDataManagement.java
@@ -0,0 +1,55 @@
+public class StudentDataManagement {
+//    instance variables
+  String name;
+  int age;
+  String address;
+
+//  constructor --> Special type of method
+  public StudentDataManagement(String name , int age , String address){
+      this.name = name;
+      this.age = age;
+      this.address = address;
+  }
+
+//  setting setters and getters
+
+  public void setName(String name){
+      this.name = name;
+  }
+
+  public  void setAge (int age){
+      this.age = age;
+  }
+
+  public  void setAddress(String address){
+      this.address = address;
+  }
+
+  public String getName(){
+      return name;
+  }
+
+  public int getAge(){
+      return age;
+  }
+
+  public  String getAddress(){
+      return  address;
+  }
+
+
+  public  String toString(){
+      return ("Student name is " + this.getName() + " , age is : " + this.getAge() + " and address is : " + this.getAddress());
+  }
+
+    public static void main(String[] args) {
+
+        StudentDataManagement john = new StudentDataManagement("john" , 25 , "23 East , blue");
+
+        System.out.println(john.getName());
+        System.out.println(john.getAddress());
+        System.out.println(john.getAge());
+
+        System.out.println(john.toString());
+    }
+}
