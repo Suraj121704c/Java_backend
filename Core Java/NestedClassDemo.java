f972b156fd22e01682b2d48f8eadabb97e945171
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-22 07:16:49 +0530

    coding

---
+++ Core Java/NestedClassDemo.java
@@ -0,0 +1,38 @@
+public class NestedClassDemo {
+
+    private  int outerData;
+
+    public void outerMethod(){
+        System.out.println("Outer Method");
+    }
+
+//  Static nested class
+    public static class staticNestedClass{
+        public void nestedMethod(){
+            System.out.println("Static nested method");
+        }
+    }
+
+//  Inner Class Or non-static nested Class
+    public class InnearClass{
+        public void innerMethod(){
+            System.out.println("Inner Method");
+        }
+    }
+    
+//    Creating innear Object
+    public void createInnerObject() {
+        InnearClass innerObject = new InnearClass();
+        innerObject.innerMethod();
+    }
+
+    public static void main(String[] args) {
+        staticNestedClass nestedObject = new staticNestedClass();
+        nestedObject.nestedMethod();
+
+
+        NestedClassDemo outerObject = new NestedClassDemo();
+        InnearClass innerObject = outerObject.new InnearClass();
+        innerObject.innerMethod();
+    }
+}
